7b1a6e167cbf87f12c7bb8289c9621b9839b1f86
Vladimir Kamarzin <vvk@vvk.pp.ru>
2019-03-06 19:04:28 +0500

    Add Amount class

--- vizbase/chains.py
+++ vizbase/chains.py
@@ -4,5 +4,7 @@ KNOWN_CHAINS = {
         "chain_id": "2040effda178d4fffff5eab7a915d4019879f5205cc5392e4bcced2b6edda0cd",
         "core_symbol": "VIZ",
         "prefix": "VIZ",
-    },
+    }
 }
+
+PRECISIONS = {"VIZ": 3, "SHARES": 6}
