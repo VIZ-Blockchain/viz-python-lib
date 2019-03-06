8569e477d954bfb368326bf466121c127e39a313
Vladimir Kamarzin <vvk@vvk.pp.ru>
2019-03-07 00:57:52 +0500

    Make TransactonBuilder work

--- viz/transactionbuilder.py
+++ viz/transactionbuilder.py
@@ -9,14 +9,13 @@ from vizbase.account import PrivateKey, PublicKey
 from vizbase.objects import Operation
 from vizbase.signedtransactions import Signed_Transaction
 
-#from .amount import Amount
-#from .asset import Asset
-#from .account import Account
-from graphenecommon.account import Account
-from graphenecommon.amount import Amount
+# We don't have own Asset class because it is unneeded
 from graphenecommon.asset import Asset
 
+from .amount import Amount
+from .account import Account
 from .instance import BlockchainInstance
+from .utils import formatTimeFromNow
 
 
 @BlockchainInstance.inject
@@ -55,3 +54,40 @@ class TransactionBuilder(GrapheneTransactionBuilder):
         self.publickey_class = PublicKey
         self.signed_transaction_class = Signed_Transaction
         self.amount_class = Amount
+
+    def constructTx(self):
+        # TODO: propose to control add_required_fees via variable into upstream?
+        """ Construct the actual transaction and store it in the class's dict
+            store
+
+            We're overriding this method because we don't need to call
+            self.add_required_fees()
+        """
+        ops = list()
+        for op in self.ops:
+            if isinstance(op, ProposalBuilder):
+                # This operation is a proposal an needs to be deal with
+                # differently
+                proposals = op.get_raw()
+                if proposals:
+                    ops.append(proposals)
+            else:
+                # otherwise, we simply wrap ops into Operations
+                ops.extend([self.operation_class(op)])
+
+        # We now wrap everything into an actual transaction
+        expiration = formatTimeFromNow(
+            self.expiration
+            or self.blockchain.expiration
+            or 30  # defaults to 30 seconds
+        )
+        ref_block_num, ref_block_prefix = self.get_block_params()
+        self.tx = self.signed_transaction_class(
+            ref_block_num=ref_block_num,
+            ref_block_prefix=ref_block_prefix,
+            expiration=expiration,
+            operations=ops,
+        )
+        dict.update(self, self.tx.json())
+        self._unset_require_reconstruction()
+
