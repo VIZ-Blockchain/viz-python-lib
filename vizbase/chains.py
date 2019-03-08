858fc739c5b412fa8a3b1506ca4d9f4fa0bc70bf
Vladimir Kamarzin <vvk@vvk.pp.ru>
2019-03-09 00:27:18 +0500

    Add shares_symbol

--- vizbase/chains.py
+++ vizbase/chains.py
@@ -3,6 +3,7 @@ KNOWN_CHAINS = {
     "VIZ": {
         "chain_id": "2040effda178d4fffff5eab7a915d4019879f5205cc5392e4bcced2b6edda0cd",
         "core_symbol": "VIZ",
+        "shares_symbol": "SHARES",
         "prefix": "VIZ",
     }
 }
