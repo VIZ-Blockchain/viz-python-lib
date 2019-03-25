6e7016bc16b51af4daead8c8d225dbfb619bdace
Vladimir Kamarzin <vvk@vvk.pp.ru>
2019-03-25 23:03:37 +0500

    No need to override appendSigner() anymore
    Upstream resolved the issue of overriding permission types.

--- viz/transactionbuilder.py
+++ viz/transactionbuilder.py
@@ -45,6 +45,8 @@ class TransactionBuilder(GrapheneTransactionBuilder):
         operations and signers.
     """
 
+    permission_types = ["active", "owner", "posting"]
+
     def define_classes(self):
         self.account_class = Account
         self.asset_class = Asset
@@ -59,46 +61,3 @@ class TransactionBuilder(GrapheneTransactionBuilder):
         """ Override this method because steem-like chains doesn't have transaction feed
         """
         return ops
-
-    def appendSigner(self, accounts, permission):
-        """ Try to obtain the wif key from the wallet by telling which account
-            and permission is supposed to sign the transaction
-        """
-        # TODO: override this class until
-        # https://github.com/xeroc/python-graphenelib/issues/85 is fixed
-        assert permission in ["posting", "active", "owner"], "Invalid permission"
-
-        if self.blockchain.wallet.locked():
-            raise WalletLocked()
-        if not isinstance(accounts, (list, tuple, set)):
-            accounts = [accounts]
-
-        for account in accounts:
-            # Now let's actually deal with the accounts
-            if account not in self.signing_accounts:
-                # is the account an instance of public key?
-                if isinstance(account, self.publickey_class):
-                    self.appendWif(
-                        self.blockchain.wallet.getPrivateKeyForPublicKey(str(account))
-                    )
-                # ... or should we rather obtain the keys from an account name
-                else:
-                    accountObj = self.account_class(
-                        account, blockchain_instance=self.blockchain
-                    )
-                    required_treshold = accountObj[permission]["weight_threshold"]
-                    keys = self._fetchkeys(
-                        accountObj, permission, required_treshold=required_treshold
-                    )
-                    # If we couldn't find an active key, let's try overwrite it
-                    # with an owner key
-                    if not keys and permission != "owner":
-                        keys.extend(
-                            self._fetchkeys(
-                                accountObj, "owner", required_treshold=required_treshold
-                            )
-                        )
-                    for x in keys:
-                        self.appendWif(x[0])
-
-                self.signing_accounts.append(account)
