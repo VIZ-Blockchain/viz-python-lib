6ee77f60eeb9a67252d2b50433aaeca8d409b584
Pavel Tanchev <dcopm999@gmail.com>
2020-04-18 16:10:06 +0500

    code_quality_tools
    * add flake8 config
    * add pre-commit to requirements.base.txt
    * setup.py: correcting with linter
    * add pre-commit config file
    * add isort config file
    * .gitignore: add exclude for mpyp cache

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 import re
+import subprocess
 import sys
 
 try:
@@ -11,74 +12,70 @@ except ImportError:
 
 
 def get_version(*file_paths):
-    """Retrieves the version from viz-python-lib/__init__.py"""
+    """Retrieves the version from viz-python-lib/__init__.py."""
     filename = os.path.join(os.path.dirname(__file__), *file_paths)
     version_file = open(filename).read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         return version_match.group(1)
-    raise RuntimeError('Unable to find version string.')
+    raise RuntimeError("Unable to find version string.")
 
 
-version = '0.1.0'
+version = "0.1.0"
 
 
-if sys.argv[-1] == 'publish':
+if sys.argv[-1] == "publish":
     try:
         import wheel
+
         print("Wheel version: ", wheel.__version__)
     except ImportError:
         print('Wheel library missing. Please run "pip install wheel"')
         sys.exit()
-    os.system('python setup.py sdist upload')
-    os.system('python setup.py bdist_wheel upload')
+    subprocess.call("python setup.py sdist upload")
+    subprocess.call("python setup.py bdist_wheel upload")
     sys.exit()
 
-if sys.argv[-1] == 'tag':
+if sys.argv[-1] == "tag":
     print("Tagging the version on git:")
-    os.system("git tag -a %s -m 'version %s'" % (version, version))
-    os.system("git push --tags")
+    subprocess.call("git tag -a %s -m 'version %s'" % (version, version))
+    subprocess.call("git push --tags")
     sys.exit()
 
-readme = open('README.md').read()
+readme = open("README.md").read()
 # history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
-    name='viz-python-lib',
+    name="viz-python-lib",
     version=version,
     description="""Python Library for VIZ""",
-    long_description=readme + '\n\n', # + history,
-    author='Vladimir Kamarzin',
+    long_description=readme + "\n\n",  # + history,
+    author="Vladimir Kamarzin",
     # author_email='On1x',
-    url='https://github.com/VIZ-Blockchain/viz-python-lib.git',
-    packages=[
-        'viz',
-        'vizapi',
-        'vizbase',
-    ],
+    url="https://github.com/VIZ-Blockchain/viz-python-lib.git",
+    packages=["viz", "vizapi", "vizbase"],
     include_package_data=True,
     install_requires=[
-        'graphenelib>=1.1.11',
-        'pycryptodome',
-        'appdirs',
-        'Events',
-        'scrypt',
-        'pyyaml',
-        'toolz',
-        'funcy',
+        "graphenelib>=1.1.11",
+        "pycryptodome",
+        "appdirs",
+        "Events",
+        "scrypt",
+        "pyyaml",
+        "toolz",
+        "funcy",
     ],
     license="MIT",
     zip_safe=False,
-    keywords='viz blockchain python library',
+    keywords="viz blockchain python library",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Framework :: graphenelib :: 1.1.11',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 3 - Alpha",
+        "Framework :: graphenelib :: 1.1.11",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
