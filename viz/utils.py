a4ad997838dd4154c8fcdc1fa2e683958b5b5143
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-05-22 23:01:44 +0500

    Add time-related utils functions

--- viz/utils.py
+++ viz/utils.py
@@ -1,15 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
+from datetime import datetime
 
-# Load methods from graphene and provide them to viz
-from graphenecommon.utils import (
-    assets_from_string,
-    formatTime,
-    formatTimeFromNow,
-    formatTimeString,
-    parse_time,
-    timeFormat,
-)
 from toolz import assoc, update_in
 
 
@@ -22,3 +14,19 @@ def json_expand(json_op, key_name="json"):
             return assoc(json_op, key_name, {})
 
     return json_op
+
+
+def time_elapsed(event_time):
+    """Takes a string time from blockchain event, and returns a time delta from now."""
+    if isinstance(event_time, str):
+        event_time = parse_time(event_time)
+    return datetime.utcnow() - event_time
+
+
+def parse_time(event_time):
+    """Take a string representation of time from the blockchain, and parse it into datetime object."""
+    return datetime.strptime(event_time, '%Y-%m-%dT%H:%M:%S')
+
+
+def time_diff(time1, time2):
+    return parse_time(time1) - parse_time(time2)
