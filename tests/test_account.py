7738fa0e9b887a4b7f6571b5c00172f78512bd42
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-05-22 23:06:33 +0500

    Refactor account.py

---
+++ tests/test_account.py
@@ -0,0 +1,85 @@
+import time
+
+import pytest
+
+from viz.account import Account
+from viz.exceptions import AccountDoesNotExistsException
+
+
+@pytest.fixture()
+def account(viz, default_account):
+    return Account(default_account)
+
+
+@pytest.fixture(scope='session')
+def _make_ops(viz, default_account):
+    # Sent to different destinations to avoid transaction dupe check fail
+    dests = ['null', 'alice', 'bob']
+    for dest in dests:
+        viz.transfer(dest, 1, "VIZ", memo="test", account=default_account)
+
+    viz.transfer_to_vesting(10, account=default_account)
+    time.sleep(1)
+
+
+def test_init(viz, default_account):
+    Account(default_account)
+
+    with pytest.raises(AccountDoesNotExistsException):
+        Account('dfsdsffdfdfd')
+
+
+def test_balances(account):
+    balance = account.balances
+    assert balance['VIZ'] > 0
+
+
+def test_energy(account):
+    en = account.energy
+    assert 0 < en <= 100
+
+
+def test_current_energy(account, viz):
+    en = account.current_energy()
+    assert 0 < en <= 100
+
+    pct = 10
+    viz.award(account.name, pct, account=account.name)
+    en_new = account.current_energy()
+    time.sleep(1)
+    assert en - en_new == pytest.approx(pct, abs=1)
+
+
+def test_virtual_op_count(viz):
+    account = Account('committee')
+    count = account.virtual_op_count()
+    assert count > 0
+
+
+def test_get_withdraw_routes(viz):
+    name = 'alice'
+    account = Account(name)
+    viz.set_withdraw_vesting_route("bob", account=name)
+    routes = account.get_withdraw_routes()
+    assert routes[0]['from_account'] == name
+
+
+@pytest.mark.usefixtures('_make_ops')
+def test_history(account):
+    history = list(account.history())
+    assert 'trx_id' in history[0]
+
+    history = list(account.history(raw_output=True))
+    assert 'trx_id' in history[0][1]
+
+    history = list(account.history(batch_size=1, limit=2))
+    assert len(history) == 2
+
+    history = list(account.history(filter_by='transfer_to_vesting'))
+    assert history[0]['type'] == 'transfer_to_vesting'
+
+
+@pytest.mark.usefixtures('_make_ops')
+def test_history_reverse(account):
+    history = list(account.history_reverse(batch_size=1, limit=2))
+    assert len(history) == 2
--- viz/account.py
+++ viz/account.py
@@ -1,15 +1,17 @@
-import time
-from contextlib import suppress
+from typing import TYPE_CHECKING, Generator, List, Optional, Union
 
-from funcy import get_in
 from toolz import dissoc
 
 from .amount import Amount
 from .blockchain import Blockchain
-from .converter import Converter
 from .exceptions import AccountDoesNotExistsException
 from .instance import shared_blockchain_instance
-from .utils import json_expand, parse_time
+from .utils import json_expand, parse_time, time_elapsed
+
+if TYPE_CHECKING:
+    from .viz import Client  # noqa: F401
+
+HistoryGenerator = Generator[Union[dict, list], None, int]
 
 
 class Account(dict):
@@ -21,157 +23,124 @@ class Account(dict):
              instance
     """
 
-    def __init__(self, account_name, blockchain_instance=None):
+    def __init__(self, account_name: str, blockchain_instance: Optional['Client'] = None) -> None:
         self.blockchain_instance = blockchain_instance or shared_blockchain_instance()
         self.name = account_name
 
-        # caches
-        self._converter = None
-
         self.refresh()
 
-    def _get_account(self, name, **kwargs):
-        """
-        Get full account details from account name.
+    @property
+    def balances(self):
+        """Shortcut to :py:func:`get_balances`"""
+        return self.get_balances()
 
-        :param str name: Account name
-        """
-        return self.blockchain_instance.rpc.get_accounts([name])[0]
+    @property
+    def energy(self):
+        """Account energy at the moment of last use (stale)"""
+        cfg = self.blockchain_instance.rpc.config
+        return self['energy'] / cfg['CHAIN_1_PERCENT']
 
     def refresh(self):
-        account = self._get_account(self.name)
-        if not account:
+        """Loads account object from blockchain."""
+        try:
+            account = self.blockchain_instance.rpc.get_accounts([self.name])[0]
+        except IndexError:
             raise AccountDoesNotExistsException
 
         # load json_metadata
         account = json_expand(account, "json_metadata")
         super(Account, self).__init__(account)
 
-    def __getitem__(self, key):
-        return super(Account, self).__getitem__(key)
+    def get_balances(self) -> dict:
+        """
+        Obtain account balances.
 
-    def items(self):
-        return super(Account, self).items()
-
-    @property
-    def converter(self):
-        if not self._converter:
-            self._converter = Converter(blockchain_instance=self.blockchain_instance)
-        return self._converter
-
-    @property
-    def profile(self):
-        with suppress(TypeError):
-            return get_in(self, ["json_metadata", "profile"], default={})
-
-    @property
-    def shares_as_core(self):
-        shares = Amount(self["vesting_shares"]).amount
-        # TODO: round or use Decimal in Converter
-        return self.converter.shares_to_core(shares)
-
-    @property
-    def balances(self):
-        return self.get_balances()
-
-    def get_balances(self):
+        :return: dict with balances like ``{'VIZ': 49400000.0, 'SHARES': 0.0}``
+        """
         balance = Amount(self["balance"])
         vesting = Amount(self["vesting_shares"])
+
         return {balance.symbol: balance.amount, vesting.symbol: vesting.amount}
 
-    def energy(self):
-        return self["energy"] / 100
+    def current_energy(self) -> float:
+        """Returns current account energy (actual data, counts regenerated energy)"""
+        self.refresh()
+        last_vote_time = parse_time(self['last_vote_time'])
+        elapsed_time = time_elapsed(last_vote_time)
+        cfg = self.blockchain_instance.rpc.config
+        regenerated_energy = (
+            cfg['CHAIN_100_PERCENT'] * elapsed_time.total_seconds() / cfg['CHAIN_ENERGY_REGENERATION_SECONDS']
+        )
+        current_energy = self['energy'] + regenerated_energy
+        energy = min(current_energy, cfg['CHAIN_100_PERCENT']) / cfg['CHAIN_1_PERCENT']
 
-    def get_followers(self, limit: int = None, offset: str = None):
-        # TODO: is this needed?
-        return [x["follower"] for x in self._get_followers(direction="follower", limit=limit, offset=offset)]
+        return energy
 
-    def get_following(self, limit: int = None, offset: str = None):
-        # TODO: is this needed?
-        return [x["following"] for x in self._get_followers(direction="following", limit=limit, offset=offset)]
-
-    def _get_followers(self, direction="follower", limit=None, offset=""):
-        # TODO: is this needed?
-        users = []
-
-        get_users = {
-            "follower": self.blockchain_instance.get_followers,
-            "following": self.blockchain_instance.get_following,
-        }[direction]
-
-        limit = limit or 10 ** 6
-        max_request_limit = 100
-        left_number = limit
-
-        while left_number > 0:
-            select_limit = min(left_number, max_request_limit)
-            result = get_users(self.name, offset, "blog", select_limit)
-            users.extend(result)
-
-            has_next = len(users) < limit and len(result) >= select_limit
-            if has_next:
-                if users:
-                    del users[-1]
-                offset = result[-1][direction]
-
-                left_number = left_number - len(result) + 1
-            else:
-                left_number = 0
-
-        return users
-
-    def virtual_op_count(self):
+    def virtual_op_count(self) -> int:
+        """Returns number of virtual ops performed by this account."""
         try:
             last_item = self.blockchain_instance.rpc.get_account_history(self.name, -1, 0)[0][0]
         except IndexError:
             return 0
         else:
-            return last_item
+            # fist item has 0 index, so count is +1
+            return last_item + 1
 
-    def get_withdraw_routes(self):
-        return self.blockchain_instance.rpc.get_withdraw_routes(self.name, "all")
+    def get_withdraw_routes(self, type_: str = 'all') -> dict:
+        """
+        Get vesting withdraw routes.
 
-    @staticmethod
-    def filter_by_date(items, start_time, end_time=None):
-        start_time = parse_time(start_time).timestamp()
-        if end_time:
-            end_time = parse_time(end_time).timestamp()
-        else:
-            end_time = time.time()
+        :param type_: route type, one of `all`, `incoming`, `outgoing`
+        :return: list with routes
 
-        filtered_items = []
-        for item in items:
-            item_time = None
-            if "time" in item:
-                item_time = item["time"]
-            elif "timestamp" in item:
-                item_time = item["timestamp"]
+        Example return:
 
-            if item_time:
-                timestamp = parse_time(item_time).timestamp()
-                if end_time > timestamp > start_time:
-                    filtered_items.append(item)
+        .. code-block:: python
 
-        return filtered_items
+            [
+                {
+                    'from_account': 'alice',
+                    'to_account': 'bob',
+                    'percent': 10000,
+                    'auto_vest': False
+                }
+            ]
+        """
+        return self.blockchain_instance.rpc.get_withdraw_routes(self.name, type_)
 
     def get_account_history(
-        self, index, limit, start=None, stop=None, order=-1, filter_by=None, raw_output=False,
-    ):
+        self,
+        index: int,
+        limit: int,
+        start: Optional[int] = None,
+        stop: Optional[int] = None,
+        order: int = -1,
+        filter_by: Optional[Union[str, List[str]]] = None,
+        raw_output: bool = False,
+    ) -> HistoryGenerator:
         """
-        A generator over steemd.get_account_history.
+        A generator over get_account_history RPC.
 
         It offers serialization, filtering and fine grained iteration control.
 
-        Args:
-            index (int): start index for get_account_history
-            limit (int): How many items in account history will be scanned (any ops, not only filtered)
-            start (int): (Optional) skip items until this index
-            stop (int): (Optional) stop iteration early at this index
-            order: (1, -1): 1 for chronological, -1 for reverse order
-            filter_by (str, list): filter out all but these operations
-            raw_output (bool): (Defaults to False). If True, return history in
-                steemd format (unchanged).
+        .. note::
+
+            This method is mostly for internal use, probably you need :py:func:`history`.
+
+        :param int index: start index for get_account_history
+        :param int limit: How many items in account history will be scanned (any ops, not only filtered)
+        :param int start: (Optional) skip items until this index
+        :param int stop: (Optional) stop iteration early at this index
+        :param order: (1, -1): 1 for chronological, -1 for reverse order
+        :param str,list filter_by: filter out all but these operations
+        :param bool raw_output: (Defaults to False). If True, return history in
+            steemd format (unchanged).
         """
+        if isinstance(filter_by, str):
+            filter_by = [filter_by]
+
+        op_count = 0
+
         history = self.blockchain_instance.rpc.get_account_history(self.name, index, limit)
         for item in history[::order]:
             index, event = item
@@ -181,9 +150,10 @@ class Account(dict):
                 continue
 
             if stop and index > stop:
-                return
+                return op_count
 
             op_type, op = event["op"]
+            # removes specified key from dict
             block_props = dissoc(event, "op")
 
             def construct_op(account_name):
@@ -200,27 +170,82 @@ class Account(dict):
                 immutable.update({"_id": _id, "index": index})
                 return immutable
 
-            if filter_by is None:
+            if filter_by is None or op_type in filter_by:
                 yield construct_op(self.name)
-            else:
-                if type(filter_by) is list:
-                    if op_type in filter_by:
-                        yield construct_op(self.name)
+                op_count += 1
 
-                if type(filter_by) is str:
-                    if op_type == filter_by:
-                        yield construct_op(self.name)
+        return op_count
 
-    def history(self, filter_by=None, start=0, batch_size=1000, raw_output=False):
-        """Stream account history in chronological order."""
+    def history(
+        self,
+        filter_by: Optional[Union[str, List[str]]] = None,
+        start: int = 0,
+        batch_size: int = 1000,
+        raw_output: bool = False,
+        limit: int = -1,
+    ) -> HistoryGenerator:
+        """
+        Stream account history in chronological order.
+
+        This generator yields history items which may be in list or dict form depending on ``raw_output``.
+
+        :param str,list filter_by: filter out all but these operations
+        :param int start: (Optional) skip items until this index
+        :param int batch_size: (Optional) request as many items from API in each chunk
+        :param bool raw_output: (Defaults to False). If True, return history in
+            steemd format (unchanged).
+        :param int limit: (Optional) limit number of filtered items to this amount (-1 means unlimited).
+            This is a rough limit, actual results could be a bit longer
+        :return: number of ops
+
+        Non-raw output example of yielded item:
+
+        .. code-block:: python
+
+            {
+                'from': 'viz',
+                'to': 'null',
+                'amount': '1.000 VIZ',
+                'memo': 'test',
+                'trx_id': '592010ade718c91a81cba3b8378c35ed81d23f23',
+                'block': 5,
+                'trx_in_block': 0,
+                'op_in_trx': 0,
+                'virtual_op': 0,
+                'timestamp': '2020-05-19T08:10:47',
+                'account': 'viz',
+                'type': 'transfer',
+                '_id': 'd1ed77ae861bb1ecc26a82dd275cc80e5ac124a6',
+                'index': 0,
+            }
+
+        Raw output example of single item:
+
+        .. code-block:: python
+
+            [
+                0,
+                {
+                    'trx_id': '592010ade718c91a81cba3b8378c35ed81d23f23',
+                    'block': 5,
+                    'trx_in_block': 0,
+                    'op_in_trx': 0,
+                    'virtual_op': 0,
+                    'timestamp': '2020-05-19T08:10:47',
+                    'op': ['transfer', {'from': 'viz', 'to': 'null', 'amount': '1.000 VIZ', 'memo': 'test'}],
+                },
+            ]
+        """
+        op_count = 0
+
         max_index = self.virtual_op_count()
         if not max_index:
-            return
+            return op_count
 
         start_index = start + batch_size
         i = start_index
         while i < max_index + batch_size:
-            yield from self.get_account_history(
+            count = yield from self.get_account_history(
                 index=i,
                 limit=batch_size,
                 start=i - batch_size,
@@ -230,51 +255,51 @@ class Account(dict):
                 raw_output=raw_output,
             )
             i += batch_size + 1
+            op_count += count
 
-    def history_reverse(self, filter_by=None, batch_size=1000, raw_output=False):
-        """Stream account history in reverse chronological order."""
+            if limit > 0 and op_count >= limit:
+                break
+
+        return op_count
+
+    def history_reverse(
+        self,
+        filter_by: Optional[Union[str, List[str]]] = None,
+        batch_size: int = 1000,
+        raw_output: bool = False,
+        limit: int = -1,
+    ) -> HistoryGenerator:
+        """
+        Stream account history in reverse chronological order.
+
+        This generator yields history items which may be in list or dict form depending on ``raw_output``.
+        Output is similar to :py:func:`history`.
+
+        :param str,list filter_by: filter out all but these operations
+        :param int batch_size: (Optional) request as many items from API in each chunk
+        :param bool raw_output: (Defaults to False). If True, return history in
+            steemd format (unchanged).
+        :param int limit: (Optional) limit number of filtered items to this amount (-1 means unlimited).
+            This is a rough limit, actual results could be a bit longer
+        :return: number of ops
+        """
+        op_count = 0
+
         start_index = self.virtual_op_count()
         if not start_index:
-            return
+            return op_count
 
         i = start_index
         while i > 0:
             if i - batch_size < 0:
                 batch_size = i
-            yield from self.get_account_history(
+            count = yield from self.get_account_history(
                 index=i, limit=batch_size, order=-1, filter_by=filter_by, raw_output=raw_output,
             )
             i -= batch_size + 1
+            op_count += count
 
-    def rawhistory(self, first=99999999999, limit=-1, only_ops=[], exclude_ops=[]):
-        """
-        Returns a generator for individual account transactions. The latest operation will be first. This call can be
-        used in a ``for`` loop.
+            if limit > 0 and op_count >= limit:
+                break
 
-        :param str account: account name to get history for
-        :param int first: sequence number of the first transaction to return
-        :param int limit: limit number of filtered operations to return
-        :param array only_ops: Limit generator by these operations
-        """
-        cnt = 0
-        _limit = 100
-        if _limit > first:
-            _limit = first
-        while first > 0:
-            # RPC call
-            txs = self.blockchain_instance.rpc.get_account_history(self.name, first, _limit)
-            for i in txs[::-1]:
-                if exclude_ops and i[1]["op"][0] in exclude_ops:
-                    continue
-                if not only_ops or i[1]["op"][0] in only_ops:
-                    cnt += 1
-                    yield i
-                    if limit >= 0 and cnt >= limit:
-                        break
-            if limit >= 0 and cnt >= limit:
-                break
-            if len(txs) < _limit:
-                break
-            first = txs[0][0] - 1  # new first
-            if _limit > first:
-                _limit = first
+        return op_count
