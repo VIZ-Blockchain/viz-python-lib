8f18c8904b8e4d1ab50d24b1e415e212cf7bf7cb
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-06-05 13:30:37 +0500

    Split assertion

--- tests/test_blockchain.py
+++ tests/test_blockchain.py
@@ -1,5 +1,6 @@
-from viz.blockchain import Blockchain
 import pytest
+
+from viz.blockchain import Blockchain
 
 
 @pytest.fixture()
@@ -26,7 +27,8 @@ def test_stream_real_ops(blockchain, viz, default_account):
     current = blockchain.get_current_block_num()
     viz.transfer('alice', 1, "VIZ", memo="test stream", account=default_account)
     ops = list(blockchain.stream(start_block=current, end_block=current + 3, filter_by='transfer'))
-    assert ops[0]['from'] == default_account and ops[0]['memo'] == 'test stream'
+    assert ops[0]['from'] == default_account
+    assert ops[0]['memo'] == 'test stream'
 
     ops = list(blockchain.stream(start_block=current + 3, end_block=current))
     assert ops
